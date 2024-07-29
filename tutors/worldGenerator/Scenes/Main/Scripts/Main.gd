0b2e15cb7a02ed7ad68a9fbafd8830d9902a28cd
midavgigo <ddd20162004@gmail.com>
2024-07-29 22:10:39 +0300

    Небольшая доработка гайда с генерацией. Добавил использование групп

--- tutors/worldGenerator/Scenes/Main/Scripts/Main.gd
+++ tutors/worldGenerator/Scenes/Main/Scripts/Main.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @onready var platform_scene = load("res://Scenes/Platform/Platform.tscn")
 
-func _ready():
+func generate():
 	for x in range(10):
 		for y in range(10):
 			var platform = platform_scene.instantiate()
@@ -10,3 +10,13 @@ func _ready():
 			platform.position.y = y*100
 			if randi()%2:
 				add_child(platform)
+
+func _ready():
+	generate()
+	
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		var platforms = get_tree().get_nodes_in_group("platform")
+		for i in platforms:
+			i.queue_free()
+		generate()
