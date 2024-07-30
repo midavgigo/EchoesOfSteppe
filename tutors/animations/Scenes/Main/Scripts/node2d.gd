9b929bad44c95c4731088ba6d8b3281dba9ced15
midavgigo <ddd20162004@gmail.com>
2024-07-30 19:41:34 +0300

    Добавил описание обучения для анимации

--- tutors/animations/Scenes/Main/Scripts/node2d.gd
+++ tutors/animations/Scenes/Main/Scripts/node2d.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
-@onready var animated_sprite = $AS2D
+@onready var animated_sprite = $AS2D #Загружаем в переменную объект по имени
 
-var play = false
+var play = false #Переменная для проигрывания анимации
 
 func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		play = not play
+	if Input.is_action_just_pressed("ui_accept"):#Автоматически добавленное событие, срабатывает на пробел и ентер
+		play = not play#Меняем переменную
 	if play:
-		animated_sprite.play("animation")
+		animated_sprite.play("animation")#Запускаем анимацию "animation"
 	else:
-		animated_sprite.stop()
+		animated_sprite.stop()#Останавливаем
