501c406e23b1584ae1730ca647d804ac079b4e5c
midavgigo <ddd20162004@gmail.com>
2024-07-30 21:17:55 +0300

    Добавил описание для гайда по файловой системе

--- tutors/filesystem/Scenes/Main/Scripts/Main.gd
+++ tutors/filesystem/Scenes/Main/Scripts/Main.gd
@@ -3,11 +3,11 @@ extends Node2D
 @onready var text_edit = $TextEdit
 @onready var text_read = $RichTextLabel
 
-func _write_file():
+func _write_file():#функция обработки сигнала с кнопки записи
 	var file = FileAccess.open("user://file.txt", FileAccess.WRITE)
 	file.store_string(text_edit.text)
 
 
-func _read_file():
+func _read_file():#функция обработки сигнала с кнопки чтения
 	var file = FileAccess.open("user://file.txt", FileAccess.READ)
 	text_read.text = file.get_as_text()
