26c6e2c1ed6bcdaf1ab17096f782257d875439f6
midavgigo <ddd20162004@gmail.com>
2024-08-04 14:54:19 +0300

    Добавил возможность атаковать на кнопку A на геймпаде, изменил поведение области боли, добавил кубы для битья

--- game/Player/Scripts/WeaponClass.gd
+++ game/Player/Scripts/WeaponClass.gd
@@ -5,15 +5,18 @@ class WeaponClass:
 	var name
 	var material
 	var type
-	var speed
+	var speed_q
 	var damage
+	var speed
+	var armor
+	var flesh
+	var spirit
 	
-
 	func debug():
 		return 	"name: "+name+\
 				"\nmaterial: "+material+\
 				"\ntype: "+type+\
-				"\nspeed: "+str(speed)+\
+				"\nspeed_q: "+str(speed_q)+\
 				"\ndamage: "+str(damage)+"\n"
 	
 	func _init(name):
@@ -24,5 +27,17 @@ class WeaponClass:
 			TYPE_DICTIONARY:
 				material = dict[name]["material"]
 				type = dict[name]["type"]
-				speed = dict[name]["speed"]
+				speed_q = dict[name]["speed_q"]
 				damage = dict[name]["damage"]
+		file = FileAccess.open("res://Player/Configurations/weapon_types.json", FileAccess.READ)
+		dict = JSON.parse_string(file.get_as_text())
+		match typeof(dict):
+			TYPE_DICTIONARY:
+				speed = dict[type]["speed"]
+		file = FileAccess.open("res://Player/Configurations/weapon_materials.json", FileAccess.READ)
+		dict = JSON.parse_string(file.get_as_text())
+		match typeof(dict):
+			TYPE_DICTIONARY:
+				flesh = dict[material]["flesh"]
+				armor = dict[material]["armor"]
+				spirit = dict[material]["spirit"]
--- game/Test/Scripts/FieldOfPain.gd
+++ game/Test/Scripts/FieldOfPain.gd
@@ -1,5 +1,20 @@
 extends Area2D
 
+var bod = null
+
+func _on_body_exited(body):
+	bod = null
 
 func _on_body_entered(body):
-	body.hit(0.1, "beast")
+	bod = body
+	
+var sum = 0
+	
+func _process(delta):
+	sum += delta
+	if sum >= 0.1:
+		if bod != null:
+			bod.set_hit(0.1, "beast")
+		sum = 0
+		
+
