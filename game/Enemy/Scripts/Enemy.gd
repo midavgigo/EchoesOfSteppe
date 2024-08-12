741fda0aef55b2c7ff960c6ad544f6320465278a
midavgigo <ddd20162004@gmail.com>
2024-08-12 14:26:28 +0300

    Добавил врага, он следует за следом игрока. Есть нерабочая пока атака в врага

--- game/Enemy/Scripts/Enemy.gd
+++ game/Enemy/Scripts/Enemy.gd
@@ -1,1 +1,59 @@
 extends CharacterBody2D
+
+@onready var sprite = $AnimatedSprite2D
+
+var enemy
+var title
+var player
+var smells
+var target 
+
+func aggressive():
+	var space_state = get_world_2d().direct_space_state
+	var no_smell = true
+	for i in smells:
+		if i == null:
+			continue
+		var query = PhysicsRayQueryParameters2D.create(position, i.position)
+		query.exclude = [self, player]
+		var result = space_state.intersect_ray(query)
+		if not result:
+			enemy.set_target(position, i.position)
+			target = i
+			no_smell = false
+			break
+	if no_smell:
+		target = null
+		enemy.stop()
+
+func tactic():
+	pass
+
+func scared():
+	pass
+
+func _ready():
+	title = get_meta("title")
+	enemy = load("res://Enemy/Scripts/EnemyClass.gd").new(title)
+	sprite.sprite_frames = load("res://Enemy/Animations/"+title+".tres")
+	sprite.animation = "Stand"
+	sprite.play("Stand")
+	var players = get_tree().get_nodes_in_group("player")
+	if players.size() > 0:
+		player = players[0]
+
+func _process(delta):
+	var temp = get_tree().get_nodes_in_group("smell")
+	smells = []
+	for i in range(16):
+		smells.append(null)
+	for i in temp:
+		smells[i.freshness] = i	
+	call(enemy.movement.pattern)
+	enemy.calc(delta)
+	velocity = enemy.get_velocity()
+	if velocity.length() >= 30:
+		sprite.play("Walk")
+	else:
+		sprite.play("Stand")
+	move_and_slide()
--- game/Enemy/Scripts/EnemyClass.gd
+++ game/Enemy/Scripts/EnemyClass.gd
@@ -11,12 +11,47 @@ class EnemyClass:
 		var type
 		var style
 		var source
-	var movement
-	var attack
+	var movement = Movement.new()
+	var attack = Attack.new()
 	var health
 	var width
 	var height
+	var name
+	
+	var velocity = Vector2()
+	var acceleration = Vector2()
 	
 	func _init(name):
+		self.name = name
 		var file = FileAccess.open("res://Enemy/Configuration/enemys.json", FileAccess.READ)
-		
+		var dict = JSON.parse_string(file.get_as_text())
+		match typeof(dict):
+			TYPE_DICTIONARY:
+				health = dict[name]["health"]
+				width = dict[name]["width"]
+				height = dict[name]["height"]
+				attack.distance = dict[name]["attack"]["distance"]
+				attack.damage = dict[name]["attack"]["damage"]
+				attack.source = dict[name]["attack"]["source"]
+				attack.style = dict[name]["attack"]["style"]
+				attack.type = dict[name]["attack"]["type"]
+				movement.accel = dict[name]["movement"]["accel"]
+				movement.pattern = dict[name]["movement"]["pattern"]
+				movement.speed = dict[name]["movement"]["speed"]
+	
+	func set_target(pos, tar):
+		var vec = (tar - pos).normalized()
+		vec *= movement.accel
+		acceleration = vec
+	
+	func stop():
+		acceleration = Vector2()
+	
+	func calc(delta):
+		if acceleration.length() == 0:
+			acceleration = -velocity*13.5
+		velocity += acceleration*delta
+		velocity = velocity.limit_length(movement.speed)
+			
+	func get_velocity():
+		return velocity
